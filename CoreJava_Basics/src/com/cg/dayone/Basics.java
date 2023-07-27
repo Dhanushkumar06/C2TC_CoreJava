849cda0f5431b0a228282add46ced8b3360b1c8c
Acer <Acer@Dhanush-PC>
2023-07-27 10:47:05 +0530

    First Commit

---
+++ CoreJava_Basics/src/com/cg/dayone/Basics.java
@@ -0,0 +1,23 @@
+package com.cg.dayone;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		int a = 20, b = 10;
+
+		  int sum = a + b;
+		  System.out.println("Sum is: " + sum);
+		  
+		  int dif = a - b;
+		  System.out.println("Diff is : " + dif);
+
+		  int mul = a * b;
+		  System.out.println("Multiple is : " + mul);
+
+		  //Division Operation
+		  int div = a / b;
+		  System.out.println("Div is : " + div);
+
+	}
+
+}
